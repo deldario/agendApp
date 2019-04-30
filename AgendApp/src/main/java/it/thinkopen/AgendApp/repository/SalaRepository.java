724baed938709c23208238c0df9763ad74b7568a
dariodeleo <dariodeleo@192.168.43.31>
2019-04-30 21:02:52 +0200

    Aggiornamento

--- AgendApp/src/main/java/it/thinkopen/AgendApp/repository/SalaRepository.java
+++ AgendApp/src/main/java/it/thinkopen/AgendApp/repository/SalaRepository.java
@@ -1,9 +1,11 @@
 package it.thinkopen.AgendApp.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import it.thinkopen.AgendApp.model.Sala;
 
+@RepositoryRestResource
 public interface SalaRepository extends CrudRepository<Sala, Long>{
 
 
