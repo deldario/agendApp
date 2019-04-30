391bd4fa6d1a9bfb9613d8ebfd076b785c89bd92
Dario De Leo <dariodeleo@MacBook-Air-di-Dario.local>
2019-04-30 15:50:29 +0200

    inizia commit project

---
+++ src/main/java/it/thinkopen/agendApp/AgendAppApplication.java
@@ -0,0 +1,13 @@
+package it.thinkopen.agendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendAppApplication.class, args);
+	}
+
+}
